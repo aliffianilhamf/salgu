5cbe9f1ffca11da33997e1f566c9e33f13d1135a
aliffianilhamf <aliffianilhamf@gmail.com>
2024-05-26 19:24:28 +0700

    fix: open file ui

--- salgu-front-end/src/app/drive/files/[id]/page.tsx
+++ salgu-front-end/src/app/drive/files/[id]/page.tsx
@@ -1,31 +1,18 @@
 "use client";
 import Button from "@/components/Button";
 import Form from "@/components/Input/Form";
-import ListFile from "@/components/ListFile";
-import ModalOpenFile from "@/components/ModalOpenFile";
 import Container from "react-bootstrap/Container";
 
 export default function File({ params }: any) {
   const id: string = params.id;
   return (
     <Container className="p-5">
-      <div className="tw-flex tw-justify-end tw-mt-20 tw-max-w-full tw-px-10">
-        {/* if you want to use onClick in the button use props onCLick, the default value is () => {} */}
-        <Button variant="tw-bg-blue-500 tw-mx-1">Share</Button>
-        <Button variant="tw-bg-blue-500 tw-mx-1">Download</Button>
-        <Button variant="tw-bg-blue-500 tw-mx-1">Delete</Button>
-        <Button variant="tw-bg-blue-500 tw-mx-1">Copy Link</Button>
+      <div
+        id="container"
+        className="tw-fixed tw-inset-0 tw-bg-black tw-bg-opacity-30 tw-backdrop-blur-sm tw-flex tw-justify-center tw-items-center  "
+      >
+        <img src="/img/horizontal.jpg" alt="" className="tw-h-3/4" />
       </div>
-      <div className="tw-flex tw-flex-col tw-max-w-6xl tw-border tw-ml-auto tw-mr-10">
-        <ListFile id={"0"}>namafile.jpg</ListFile>
-        <ListFile id={"00"}>namafile.jpg</ListFile>
-        <ListFile id={"000"}>namafile.jpg</ListFile>
-        <ListFile id={"0000"}>namafile.jpg</ListFile>
-        <ListFile id={"00000"}>namafile.jpg</ListFile>
-        <ListFile id={"000000"}>namafile.jpg</ListFile>
-      </div>
-
-      <ModalOpenFile></ModalOpenFile>
     </Container>
   );
 }
