2fe1969a787e8d1de72f04827ea56ddb37855ac0
pearlgw <gayuhterluv@gmail.com>
2024-05-31 13:43:27 +0700

    feat: delete multiple

--- salgu-front-end/src/app/drive/files/[id]/actions.ts
+++ salgu-front-end/src/app/drive/files/[id]/actions.ts
@@ -6,3 +6,13 @@ export const getFileHistory = async (id: string) => {
   const res = await api.get(`/files/${id}/history`);
   return res.data;
 };
+
+export const deleteFilesById = async (ids: string[]) => {
+  const deletePromises = ids.map(id => api.delete(`${process.env.NEXT_PUBLIC_API_URL}/files/${id}`));
+  try {
+    await Promise.all(deletePromises);
+    console.log('All selected files have been deleted');
+  } catch (error) {
+    console.error('Error deleting files:', error);
+  }
+}
--- salgu-front-end/src/app/drive/folders/[id]/actions.ts
+++ salgu-front-end/src/app/drive/folders/[id]/actions.ts
@@ -6,3 +6,13 @@ export const getDir = async (id: string) => {
   const res = await api.get(`/dirs/${id}`);
   return res.data;
 };
+
+export const deleteDirsById = async (ids: string[]) => {
+  const deletePromises = ids.map(id => api.delete(`${process.env.NEXT_PUBLIC_API_URL}/dirs/${id}`));
+  try {
+    await Promise.all(deletePromises);
+    console.log('All selected directories have been deleted');
+  } catch (error) {
+    console.error('Error deleting directories:', error);
+  }
+}
