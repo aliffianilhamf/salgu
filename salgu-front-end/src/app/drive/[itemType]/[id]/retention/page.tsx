d0d406270a8d473cb3a1daf2a5d4c6acc3222391
Ulima Muna Syarifah <ulimamuna325@gmail.com>
2024-06-19 15:03:03 +0700

    feature: retention page

---
+++ salgu-front-end/src/app/drive/[itemType]/[id]/retention/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { NextPage } from "next";
+import DeletedItems from "@/components/DeletedItems";
+
+const RetentionPage: NextPage = () => {
+  return (
+    <div className="tw-bg-gray-100 tw-min-h-screen tw-flex tw-items-center tw-justify-center">
+      <DeletedItems />
+    </div>
+  );
+};
+
+export default RetentionPage;
